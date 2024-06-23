f1494a0b445ac00770e6505094cde48786528359
cyCaffine <14111menkashah@gmail.com>
2024-06-23 18:10:31 +0530

    Time: 0 ms (100%), Space: 40.6 MB (13.55%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,31 +1,21 @@
 class Solution {
     public int fib(int n) {
-        // if(n == 0 || n == 1)return n;
-        // int n1 = fib(n-1);
-        // int  n2 = fib(n-2);
-        // return n1+n2;
+        if(n == 0){
+            return 0;
+        }
 
+        int prev1 = 1;
+        int prev2 = 0;
+
+        for(int i = 2; i <=n; i++){
+            int curr = prev1+prev2;
+
+            // ṣhift logic
+            prev2 = prev1;
+         prev1 =  curr;
+
+        }
+        return prev1;
         
-        // Handle the base cases where n is 0 or 1
-    if (n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
-    }
-
-      // Initialize the dp array to store Fibonacci numbers up to n
-    int[] dp = new int[n + 1];
-
-    // Base cases
-    dp[0] = 0;
-    dp[1] = 1;
-
-    // Fill the dp array using the bottom-up approach
-    for (int i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
-    }
-
-    // Return the nth Fibonacci number
-    return dp[n];
     }
 }
