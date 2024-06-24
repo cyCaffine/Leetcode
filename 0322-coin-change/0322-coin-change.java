31d6878e247d226d07f205759a25a7d0e0f5de3b
cyCaffine <14111menkashah@gmail.com>
2024-06-24 22:26:56 +0530

    Time: 16 ms (61.99%), Space: 44 MB (84.1%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,23 @@
+class Solution {
+    // int solveTab(int []num, int x){
+       int solveTab(int[] coins, int amount) {
+        int dp[] = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        dp[0] = 0;
+        
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        return solveTab(coins, amount);
+    }
+}
